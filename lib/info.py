7ce2b26888a5d5fd1898bad736ac77fcd3bedcbe
Alessandro Rabitti <axel83.beta@gmail.com>
2019-06-14 00:27:27 +0200

    Add concerts api query

--- lib/info.py
+++ lib/info.py
@@ -6,7 +6,15 @@ import json
 
 def get_info_search(search: str):
   artists = get_artists(search)
+  if len(artists):
+    artists_concerts = get_concerts_via_artist(str(artists[0]['id']))
+    print(artists_concerts)
+
   locations = get_locations(search)
+  if len(locations):
+    locations_concerts = get_concerts_via_artist(str(locations[0]['metroArea']['id']))
+    print(locations_concerts)
+
 
 def get_artists(artist: str):
   request_url = 'https://api.songkick.com/api/3.0/search/artists.json?query=' + artist + '&apikey=' + os.getenv("songkick_token")
@@ -14,8 +22,18 @@ def get_artists(artist: str):
   artists = json.loads(response.content.decode('utf-8'))
 
   if artists.get('resultsPage').get('totalEntries') > 0:
-    print(artists.get('resultsPage').get('results').get('artist')[0])
-    return artists.get('resultsPage').get('results').get('artist')[0]
+    return artists.get('resultsPage').get('results').get('artist')
+  else:
+    return []
+
+def get_concerts_via_artist(artist_id: str):
+  request_url = 'https://api.songkick.com/api/3.0/artists/' + artist_id + '/calendar.json?apikey=' + os.getenv("songkick_token")
+  print(request_url)
+  response = requests.get(request_url)
+  concerts = json.loads(response.content.decode('utf-8'))
+
+  if concerts.get('resultsPage').get('totalEntries') > 0:
+    return concerts.get('resultsPage').get('results').get('event')
   else:
     return []
 
@@ -25,7 +43,16 @@ def get_locations(location: str):
   locations = json.loads(response.content.decode('utf-8'))
 
   if locations.get('resultsPage').get('totalEntries') > 0:
-    print(locations.get('resultsPage').get('results').get('location')[0])
-    return locations.get('resultsPage').get('results').get('location')[0]
+    return locations.get('resultsPage').get('results').get('location')
+  else:
+    return []
+
+def get_concerts_via_location(location_id: str):
+  request_url = 'https://api.songkick.com/api/3.0/metro_areas/' + location_id + '/calendar.json?apikey=' + os.getenv("songkick_token")
+  response = requests.get(request_url)
+  concerts = json.loads(response.content.decode('utf-8'))
+
+  if concerts.get('resultsPage').get('totalEntries') > 0:
+    return concerts.get('resultsPage').get('results').get('event')
   else:
     return []
