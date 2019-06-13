2f499fc136cb724c2f1d5904e97788316615efc9
Alessandro Rabitti <axel83.beta@gmail.com>
2019-06-13 23:47:45 +0200

    Improve concerts research via artists and locations

--- lib/info.py
+++ lib/info.py
@@ -1,18 +1,31 @@
+#!/usr/bin/env python3
 from dotenv import load_dotenv
 import os
 import requests
 import json
 
 def get_info_search(search: str):
-  artist = get_artist(search)
+  artists = get_artists(search)
+  locations = get_locations(search)
 
-def get_artist(artist: str):
+def get_artists(artist: str):
   request_url = 'https://api.songkick.com/api/3.0/search/artists.json?query=' + artist + '&apikey=' + os.getenv("songkick_token")
   response = requests.get(request_url)
-  artist = json.loads(response.content.decode('utf-8'))
+  artists = json.loads(response.content.decode('utf-8'))
 
-  if artist.get('resultsPage').get('totalEntries') > 0:
-    print(artist.get('resultsPage').get('results').get('artist')[0])
-    return artist.get('resultsPage').get('results').get('artist')[0]
+  if artists.get('resultsPage').get('totalEntries') > 0:
+    print(artists.get('resultsPage').get('results').get('artist')[0])
+    return artists.get('resultsPage').get('results').get('artist')[0]
+  else:
+    return []
+
+def get_locations(location: str):
+  request_url = 'https://api.songkick.com/api/3.0/search/locations.json?query=' + location + '&apikey=' + os.getenv("songkick_token")
+  response = requests.get(request_url)
+  locations = json.loads(response.content.decode('utf-8'))
+
+  if locations.get('resultsPage').get('totalEntries') > 0:
+    print(locations.get('resultsPage').get('results').get('location')[0])
+    return locations.get('resultsPage').get('results').get('location')[0]
   else:
     return []
