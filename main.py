308b62e5ebbdb0aebdfdb92ee660f0ad34487bb9
Alessandro Rabitti <axel83.beta@gmail.com>
2019-06-16 10:55:30 +0200

    Fix main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 import sys
 sys.path.append('lib/')
-import logging
 import dispatcher
+import logging
 
 
-__author__ = "Alessandro Rabitti"
-__version__ = "0.0.1"
-__license__ = "MIT"
+__author__ = 'Alessandro Rabitti'
+__version__ = '0.0.1'
+__license__ = 'MIT'
 
-logging.basicConfig(level=logging.INFO)
 
-if __name__ == "__main__":
+def main():
+    logging.basicConfig(level=logging.INFO)
     dispatcher.poll()
+
+
+if __name__ == '__main__':
+    main()
